3f37c82e125035a0b3c32e8e8f0aed8ec3bac48e
Shabeeb <shabeeboali@gmail.com>
2022-01-30 08:52:45 +0530

    init

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,8 @@
+namespace Shabeeb\Cmsbox\Providers;
+
+use Konekt\Concord\BaseBoxServiceProvider;
+
+class ModuleServiceProvider extends BaseBoxServiceProvider
+{
+
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'    => 'CMS Box',
+    'version' => '1.0.0'
+];
